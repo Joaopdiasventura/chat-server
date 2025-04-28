fd637b727635e090a1f14d602177ef6f975b19b3
João Paulo Dias <joaopdias.dev@gmail.com>
2025-04-28 05:01:26 -0300

    fix: enable the cors policy

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,17 @@ import { AppModule } from "./app.module";
 async function bootstrap(): Promise<void> {
   const app =
     await NestFactory.create<INestApplication<ExpressAdapter>>(AppModule);
+
   const configService = app.get<ConfigService>(ConfigService);
+  const corsOptions = {
+    origin: configService.get("client.url"),
+    methods: ["GET", "DELETE", "POST", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  };
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors(corsOptions);
+
   await app.listen(configService.get<number>("port"));
 }
 bootstrap();
